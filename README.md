3a82298c4d7f17840db0e608e7a5e8871420eaa1
Erlend Røsjø <erlend.rosjo@knowit.no>
2014-06-24 22:03:07 +0200

    Added instuctions, updated code to parse GH hook response

--- README.md
+++ README.md
@@ -4,3 +4,12 @@ Eager Beaver
 ![Eager Beaver](http://pi.amspictures.com/files/2012/05/Beaver-1.jpg)
 
 [Staticstack](https://github.knowit.no/kyber/staticstack) build server
+
+Testing
+-------
+
+ - Instal
+ l ngrok: https://ngrok.com/download
+ - Start local server ('node server')
+ - Unzip and start ngrok at port 8000: './ngrok 8000'
+ - Set up ngrok url (http://somethingsomething.ngrok.com) as webhook url under Service Hooks for some Staticstack-based GH repo
--- server.js
+++ server.js
@@ -1,27 +1,51 @@
 var Hapi = require('hapi');
+var Fs = require('fs');
 
 var server; 
 
 if(process.env.PORT) {
-	// Azure init
-	server = new Hapi.Server(process.env.PORT, { cors: true });
+  // Azure init
+  server = new Hapi.Server(process.env.PORT, { cors: true });
 }
 else {
-	// Create localhost server
-	server = new Hapi.Server('localhost', 8000, { cors: true });
+  // Create localhost server
+  server = new Hapi.Server('localhost', 8000, { cors: true });
 }
 
 // Add the route
 server.route({
-	method: 'GET',
-	path: '/',
-	handler: function (request, reply) {
-		console.log(request);
-		reply('Welcome to Eager Beaver 0.1');
-	}
+  method: 'GET',
+  path: '/',
+  handler: function (request, reply) {
+    console.log(request);
+    reply('Welcome to Eager Beaver 0.1');
+  }
+});
+
+server.route({
+  method: 'POST',
+  path: '/',
+  handler: function (request, reply) {
+    var payload = JSON.parse(request.payload.payload);
+
+    console.log('payload.ref:', payload.ref);
+    console.log('payload.repository.name:', payload.repository.name);
+    console.log('payload.repository.url:', payload.repository.url);
+    console.log('payload.repository.description:', payload.repository.description);
+    console.log('payload.repository.owner.name:', payload.repository.owner.name);
+    console.log('payload.repository.owner.email:', payload.repository.owner.email);
+    console.log('payload.repository.created_at:', payload.repository.created_at);
+    console.log('payload.repository.pushed_at:', payload.repository.pushed_at);
+    console.log('payload.pusher.name:', payload.pusher.name);
+    console.log('payload.head_commit.message:', payload.head_commit.message);
+
+    reply({ 
+        greeting: 'Thanks Github!'
+      });
+    }
 });
 
 // Start the server
 server.start(function () {
-	console.log("Server started at " + server.info.uri);
+  console.log("Server started at " + server.info.uri);
 });
