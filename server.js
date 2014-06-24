0a3971ca2216dcdbf356a94748335571007146e9
Erlend Røsjø <erlend.rosjo@knowit.no>
2014-06-24 20:54:19 +0200

    More azure port stuff

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 var Hapi = require('hapi');
 
-var port = process.env.PORT || 8000;
-var host = process.env.HOST || 'localhost';
-console.log("port:", port);
+var host = process.env.PORT || 'localhost';
+
 console.log("host:", host);
 
 // Create a server with a host and port
-var server = new Hapi.Server(host,port,{ cors: true });
+var server = new Hapi.Server(host, 0,{ cors: true });
 
 // Add the route
 server.route({
