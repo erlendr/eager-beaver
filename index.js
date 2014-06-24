518373b80c261cf035eedcbe1c117f9cf98705cc
Erlend Røsjø <erlend.rosjo@knowit.no>
2014-06-24 20:25:14 +0200

    Prepared for Azure deploy

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 var Hapi = require('hapi');
 
+var port = process.env.PORT || 8000;
+var host = process.env.HOST || 'localhost';
+
 // Create a server with a host and port
-var server = Hapi.createServer('localhost', 8000);
+var server = Hapi.createServer(host, port, { cors: true });
 
 // Add the route
 server.route({
   method: 'GET',
   path: '/',
   handler: function (request, reply) {
-    reply('hello world');
+    reply('Welcome to Eager Beaver 0.1');
   }
 });
 
 // Start the server
-server.start();
-console.log("Server started at port http://localhost:8000")
+server.start(function () {
+	console.log("Server started at " + server.info.uri);
+});
