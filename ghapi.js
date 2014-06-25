6a2725aefad041544675fca7f98a644b6c0fb359
Erlend Røsjø <erlend.rosjo@knowit.no>
2014-06-25 20:54:19 +0200

    Experimental GH-API archive file download

---
+++ ghapi.js
@@ -0,0 +1,37 @@
+var GitHubApi = require("github");
+var https = require('https');
+var fs = require('fs');
+
+var github = new GitHubApi({
+  // required
+  version: "3.0.0",
+  // optional
+  debug: false,
+  protocol: "https",
+  host: "github.knowit.no",
+  pathPrefix: "/api/v3", // for some GHEs
+  timeout: 5000
+});
+
+// or oauth key/ secret
+github.authenticate({
+  type: "oauth",
+  token: process.env.token
+});
+
+github.repos.getArchiveLink(
+{
+  user: "erlendr",
+  repo: "webhook-test",
+  archive_format: "tarball",
+  ref: "master"
+},
+function (err, res) {
+  console.log("res.meta.location:", res.meta.location);
+  var url = res.meta.location;
+  var file = fs.createWriteStream("temp.tar.gz");
+  var request = https.get(url, function(response) {
+    response.pipe(file);
+  });
+});
+
