61e18b143a1f7ff59c52779f4fc240085fac876e
Erlend Røsjø <erlend.rosjo@knowit.no>
2014-06-25 22:59:27 +0200

    Working archive download and inflation

--- ghapi.js
+++ ghapi.js
@@ -1,37 +1,43 @@
-var GitHubApi = require("github");
+var GitHubApi = require('github');
 var https = require('https');
 var fs = require('fs');
+var zlib = require('zlib');
+var tar = require('tar');
 
 var github = new GitHubApi({
   // required
-  version: "3.0.0",
+  version: '3.0.0',
   // optional
   debug: false,
-  protocol: "https",
-  host: "github.knowit.no",
-  pathPrefix: "/api/v3", // for some GHEs
+  protocol: 'https',
+  host: 'github.knowit.no',
+  pathPrefix: '/api/v3', // for some GHEs
   timeout: 5000
 });
 
 // or oauth key/ secret
 github.authenticate({
-  type: "oauth",
+  type: 'oauth',
   token: process.env.token
 });
 
 github.repos.getArchiveLink(
 {
-  user: "erlendr",
-  repo: "webhook-test",
-  archive_format: "tarball",
-  ref: "master"
+  user: 'erlendr',
+  repo: 'webhook-test',
+  archive_format: 'tarball',
+  ref: 'master'
 },
 function (err, res) {
-  console.log("res.meta.location:", res.meta.location);
+  console.log('res.meta.location:', res.meta.location);
   var url = res.meta.location;
-  var file = fs.createWriteStream("temp.tar.gz");
   var request = https.get(url, function(response) {
-    response.pipe(file);
+    response
+    .pipe(zlib.createGunzip())
+    .pipe(tar.Extract({
+      path: 'work/'
+    }));
+    console.log('Archive inflated');
   });
 });
 
