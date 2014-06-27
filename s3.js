21d72b9f7634ac015dcf8d27c1e4333e45cae2fc
Erlend Røsjø <erlend.rosjo@knowit.no>
2014-06-27 20:59:43 +0200

    s3 bucket sync

--- s3.js
+++ s3.js
@@ -1,8 +1,14 @@
 var AWS = require('aws-sdk');
+var s3 = require('s3');
 
 AWS.config.loadFromPath('./s3config.json');
+var awsS3Client = new AWS.S3();
 
-var s3 = new AWS.S3();
+var options = {
+  s3Client: awsS3Client,
+};
+
+var client = s3.createClient(options);
 
 var bucketName = 'eager-test';
 
@@ -11,7 +17,7 @@ console.log('Bucketname:', bucketName);
 createBucket(bucketName);
 
 function createBucket(bucketName) {
-  s3.createBucket({Bucket: bucketName}, function(err, data) {
+  awsS3Client.createBucket({Bucket: bucketName}, function(err, data) {
     if (err) {
       if(err.code === 'BucketAlreadyOwnedByYou') {
         console.log('Bucket already exists');
@@ -42,12 +48,35 @@ function putBucketWebsite(bucketName) {
     }
   };
 
-  s3.putBucketWebsite(params, function(err, data) {
+  awsS3Client.putBucketWebsite(params, function(err, data) {
     if (err) {
       console.log(err, err.stack);
     }
     else {
       console.log('Bucket website enabled');
+      sync(bucketName);
     }
+  });
+}
+
+function sync(bucketName) {
+  var params = {
+    localDir: './work/erlendr-webhook-test-c086d04',
+    deleteRemoved: false,
+    s3Params: {
+      Bucket: bucketName,
+      Prefix: ''
+    },
+  };
+
+  var uploader = client.uploadDir(params);
+  uploader.on('error', function(err) {
+    console.error('unable to sync:', err.stack);
+  });
+  uploader.on('progress', function() {
+    console.log('progress', uploader.progressAmount, uploader.progressTotal);
+  });
+  uploader.on('end', function() {
+    console.log('done uploading');
   });
 }
