d8673cfc0f4239a0612fbaff91c9d4688295c185
Erlend Røsjø <erlend.rosjo@knowit.no>
2015-01-29 13:40:28 +0100

    AWS SQS test

---
+++ queue.js
@@ -0,0 +1,67 @@
+var AWS = require('aws-sdk');
+AWS.config.loadFromPath('./s3config.json');
+var sqs = new AWS.SQS();
+
+var queueUrl = 'https://sqs.eu-west-1.amazonaws.com/342335610929/eagerbeaver';
+
+function receiveMessage() {
+  var params = {
+    QueueUrl: queueUrl, /* required */
+    AttributeNames: [
+    'Policy | VisibilityTimeout | MaximumMessageSize | MessageRetentionPeriod | ApproximateNumberOfMessages | ApproximateNumberOfMessagesNotVisible | CreatedTimestamp | LastModifiedTimestamp | QueueArn | ApproximateNumberOfMessagesDelayed | DelaySeconds | ReceiveMessageWaitTimeSeconds | RedrivePolicy',
+    /* more items */
+    ],
+    MaxNumberOfMessages: 1,
+  };
+
+  sqs.receiveMessage(params, function(err, data) {
+    if (err) {
+      console.error(err, err.stack); // an error occurred
+    }
+    else {
+      var messages = data.Messages;
+      if(messages) {
+        var firstMessage = messages[0];
+        console.log('Received message: '  + JSON.stringify(firstMessage));
+        deleteMessage(firstMessage.ReceiptHandle);
+      }
+    }
+  });
+}
+
+function deleteMessage(receiptHandle) {
+  var params = {
+    QueueUrl: queueUrl, /* required */
+    ReceiptHandle: receiptHandle /* required */
+  };
+  sqs.deleteMessage(params, function(err, data) {
+    if (err) {
+      console.error('Error deleting message:', err, err.stack); // an error occurred
+    }
+    else     {
+      console.log('Message deleted: ', data);           // successful response
+    }
+  });
+};
+
+function sendMessage(messageBody) {
+  var params = {
+    MessageBody: messageBody, /* required */
+    QueueUrl: queueUrl, /* required */
+    DelaySeconds: 0,
+  };
+
+  sqs.sendMessage(params, function(err, data) {
+    if (err) {
+      console.error(err, err.stack); // an error occurred
+    }
+    else {
+      console.log(data);           // successful response
+    }
+
+  });
+}
+
+setInterval(receiveMessage, 2000);
+
+sendMessage('TESTING-123');
