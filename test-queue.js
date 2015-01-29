05f08e70cfdf5314782f918ad862282fa91ad293
Erlend Røsjø <erlend.rosjo@knowit.no>
2015-01-29 18:52:37 +0100

    More SQS stuff

--- test-queue.js
+++ test-queue.js
@@ -7,17 +7,17 @@ var queueUrl = 'https://sqs.eu-west-1.amazonaws.com/342335610929/eagerbeaver';
 function sendMessage(messageBody) {
   console.log('sendMessage called with messageBody', messageBody);
   var params = {
-    MessageBody: messageBody, /* required */
-    QueueUrl: queueUrl, /* required */
+    MessageBody: messageBody,
+    QueueUrl: queueUrl,
     DelaySeconds: 0,
   };
 
   sqs.sendMessage(params, function(err, data) {
     if (err) {
-      console.error('sendMessage error:', err, err.stack); // an error occurred
+      console.error('sendMessage error:', err, err.stack);
     }
     else {
-      console.log('sendMessage done:', data);           // successful response
+      console.log('sendMessage done:', data);
     }
 
     process.exit();
